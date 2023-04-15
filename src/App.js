653005eebcd81347f1ca07d8fdbe7a9872ca4db4
Andraya77 <110143074+andraya77@users.noreply.github.com>
2023-04-14 22:12:48 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Lesson 3 Hands On Application
         </a>
       </header>
     </div>
